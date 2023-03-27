ec27e4219be3cb1672fb7de52e5215790b1a93d3
chswell <nikitastreltsov324@gmail.com>
2023-03-27 14:42:23 +0700

    create home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,90 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>Hello</h3>
-      </header>
+      <div className="wrapper clear">
+        <header className="d-flex justify-between align-center p-40">
+          <div className="d-flex align-center">
+            <img className="mr-20" width={40} height={40} src="img/logo.png" alt="Logo" />
+            <div>
+              <h3 className="text-uppercase">React Sneakers</h3>
+              <p className="opacity-5">Магазин кроссовок</p>
+            </div>
+          </div>
+
+          <ul className="d-flex">
+            <li className="mr-30">
+            <img className="mr-10" src="/img/cart.svg" alt="" />
+              <span>1205р.</span>
+            </li>
+            <li>
+              <img src="/img/user.svg" alt="" />
+            </li>
+          </ul>
+        </header>
+        <div className="content p-40">
+          <h1 className="mb-40">Все кроссовки</h1>
+          
+          <div className="d-flex">
+            <div className="card">
+              <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
+              <h5>Мужские Кроссовки Nike Kyrie 7</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column">
+                  <span>Цена:</span>
+                  <b>12 999 руб.</b>
+                </div>
+                <button className="button">
+                  <img width={11} height={11} src="/img/plus.svg" alt="+" />
+                </button>
+              </div>
+            </div>
+
+            <div className="card">
+              <img width={133} height={112} src="/img/sneakers/2.jpg" alt="" />
+              <h5>Мужские Кроссовки Nike Kyrie 7</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column">
+                  <span>Цена:</span>
+                  <b>12 999 руб.</b>
+                </div>
+                <button className="button">
+                  <img width={11} height={11} src="/img/plus.svg" alt="+" />
+                </button>
+              </div>
+            </div>
+
+            <div className="card">
+              <img width={133} height={112} src="/img/sneakers/3.jpg" alt="" />
+              <h5>Мужские Кроссовки Nike Kyrie 7</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column">
+                  <span>Цена:</span>
+                  <b>12 999 руб.</b>
+                </div>
+                <button className="button">
+                  <img width={11} height={11} src="/img/plus.svg" alt="+" />
+                </button>
+              </div>
+            </div>
+
+            <div className="card">
+              <img width={133} height={112} src="/img/sneakers/4.jpg" alt="" />
+              <h5>Мужские Кроссовки Nike Kyrie 7</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column">
+                  <span>Цена:</span>
+                  <b>12 999 руб.</b>
+                </div>
+                <button className="button">
+                  <img width={11} height={11} src="/img/plus.svg" alt="+" />
+                </button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      
     </div>
   );
 }
